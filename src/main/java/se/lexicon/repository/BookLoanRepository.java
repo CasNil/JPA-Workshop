241146467dfd2e1ba3ca0a8498e5449262fa3a3c
Casper Nilsson <caspernilsson13@gmail.com>
2024-09-25 11:18:00 +0200

    Created Book Entity and BookLoan Entity. Implemented CRUD Operations on both.

--- src/main/java/se/lexicon/repository/BookLoanRepository.java
+++ src/main/java/se/lexicon/repository/BookLoanRepository.java
@@ -1,6 +1,9 @@
 package se.lexicon.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import se.lexicon.entities.BookLoan;
 
 import java.time.LocalDate;
@@ -14,7 +17,13 @@ public interface BookLoanRepository extends CrudRepository<BookLoan, Integer> {
 
     List<BookLoan> findByReturnedFalse();
 
-    List<BookLoan> findByDueDateAfterAndReturnedFalse(LocalDate dueDate);
+    @Query("SELECT bl FROM BookLoan bl WHERE bl.dueDate < :currentDate AND bl.returned = false")
+    List<BookLoan> findOverdueLoans(@Param("currentDate")LocalDate currentDate);
 
     List<BookLoan> findByLoanDateBetween(LocalDate startDate, LocalDate endDate);
+
+    @Modifying
+    @Query("UPDATE BookLoan bl SET bl.returned = true WHERE bl.id = :loanId")
+    void markAsReturned(@Param("loanID") Integer loanID);
+
 }
